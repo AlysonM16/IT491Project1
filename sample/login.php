6bf73a843b6cdb4ff7486db8eab47aad348b84e4
Alyson M <alm57@njit.edu>
2026-02-02 19:59:32 -0500

    web server

---
+++ sample/login.php
@@ -0,0 +1,17 @@
+<?php
+if (!isset($_POST))
+{
+	$msg = "NO POST MESSAGE SET";
+	echo json_encode($msg);
+	exit(0);
+}
+$request = $_POST;
+$response = "unsupported request type";
+switch ($request["type"]){
+case "login":
+	$response = "login, yea we can";
+break;
+}
+echo json_encode($response);
+exit(0);
+?>
